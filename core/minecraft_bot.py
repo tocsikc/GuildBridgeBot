fafa80ad6feb85e66dc7b230590ae82b601822f5
tocsikc <131706771+tocsikc@users.noreply.github.com>
2024-09-11 15:43:39 +0100

    Add files via upload

--- core/minecraft_bot.py
+++ core/minecraft_bot.py
@@ -193,9 +193,10 @@ class MinecraftBotManager:
                     final_kill_death_ratio = roundToHundreths(final_kills_bedwars / ensureValidDenominator(final_deaths_bedwars))
                     target_user = username if len(command_args) == 1 else command_args[1]
 
-                    player_stats = f"{target_user} | WLR: {win_loss_ratio} | FKDR: {final_kill_death_ratio} | W: {wins_bedwars} | FK: {final_kills_bedwars} | WS: {winstreak_bedwars}"
-                    
-                self.send_minecraft_message(None, player_stats, "General")
+                    player_stats = f"{target_user} | WLR: {win_loss_ratio} 
+                    | FKDR: {final_kill_death_ratio} | W: {wins_bedwars} 
+                    | FK: {final_kills_bedwars} | WS: {winstreak_bedwars}"
+                    self.send_minecraft_message(None, player_stats, "General")
                 return
 
     def send_minecraft_message(self, discord, message, type):
