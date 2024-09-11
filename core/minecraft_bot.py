d31ea8547d8ec730fa75cda7bfa25e50eb7cc19b
tocsikc <131706771+tocsikc@users.noreply.github.com>
2024-09-11 15:12:11 +0100

    Add files via upload

--- core/minecraft_bot.py
+++ core/minecraft_bot.py
@@ -4,7 +4,7 @@ from javascript import require, On, config
 
 from core.config import ServerConfig, SettingsConfig, AccountConfig
 
-regex_username = re.compile(r"^(?:Guild|Officer) > (?:\[[+A-Z]*\] )*([a-zA-Z0-9_]+) (?:\[[A-Z]*\]): (.+)")
+regex_username = re.compile(r"^(?:Guild|Officer) > (?:\[[+A-Z]*\] )*([a-zA-Z0-9_]+) (?:\[[A-Z]*\]: )?(.+)")
 
 mineflayer = require("mineflayer")
 
@@ -149,6 +149,7 @@ class MinecraftBotManager:
                     "has unmuted" in message or \
                     "You're currently guild muted" in message:
                 self.send_to_discord(message)
+
 
             username, parsed_message = regex_username.match(message).groups()
             username.strip()
